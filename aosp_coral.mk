82daccfaf5da1a0c3c26f4eba8458f350678f92a
Michael Bestas <mkbestas@lineageos.org>
2022-01-20 21:47:10 +0100

    floral: Update fingerprint/build description from SQ1A.220105.002
    Change-Id: I65742d6bf9a71fb66329c23acc41f16ee4f7d9eb

--- aosp_coral.mk
+++ aosp_coral.mk
@@ -78,8 +78,8 @@ PRODUCT_MODEL := Pixel 4 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=coral \
-    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.211205.008 7888514 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.220105.002 7961164 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:12/SQ1A.211205.008/7888514:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:12/SQ1A.220105.002/7961164:user/release-keys
 
 $(call inherit-product, vendor/google/coral/coral-vendor.mk)
