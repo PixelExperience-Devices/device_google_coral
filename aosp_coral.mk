929366035a2490fe93dc4142bbcabe6619bfeab4
Michael Bestas <mkbestas@lineageos.org>
2022-04-10 00:17:35 +0300

    Update fingerprint/build description from SP2A.220405.003
    Change-Id: Ic79ca7eaeab7c499ebfad8c749550b895344e86d

--- aosp_coral.mk
+++ aosp_coral.mk
@@ -77,8 +77,8 @@ PRODUCT_MODEL := Pixel 4 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=coral \
-    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220405.003 8210211 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220405.003/8210211:user/release-keys
 
 $(call inherit-product, vendor/google/coral/coral-vendor.mk)
